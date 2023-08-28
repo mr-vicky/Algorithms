6ce0f1c42caa2f9def5df6483fa802fc6d959c1a
Vivek Sarade <viveksarade4949@gmail.com>
2023-08-28 19:14:41 +0530

    Kadanes Algorithm

---
+++ 03]. Kadanes Algorithm/01_Maximum_subarray.cpp
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/maximum-subarray/
+
+// Kadanes's Algorithms
+// TC: O(n)
+// SC: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int max_sum = nums[0];
+        for(auto it : nums){
+            sum += it;
+            max_sum = max(max_sum, sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max_sum;
+    }
+};
